f4b84de9d42d548b3c516b16571eee9e30037a2b
sonjibony <sonjibony@gmail.com>
2022-08-20 13:03:32 +0600

    calculation done

--- total.js
+++ total.js
@@ -1,27 +1,34 @@
+function calculation (elementId){
+const element = document.getElementById(elementId);
+const elementValue = parseInt (element.innerText);
+
+return elementValue;
+}
+
 document.getElementById('btn-check-out').addEventListener('click',function(){
 
-    const subtotalElement = document.getElementById('subtotal');
-    const subtotal = parseInt (subtotalElement.innerText)
-    const phonePriceElement = document.getElementById('phone-price');
-    const phonePrice = parseInt (phonePriceElement.innerText);
-    const casePriceElement = document.getElementById('case-price');
-    const casePrice = parseInt (casePriceElement.innerText);
-    
+ const casePrice= calculation ('case-price');
+const phonePrice= calculation('phone-price');
+
+  //subtotal-------
+let subtotalElement = document.getElementById('subtotal');
+let subtotal = parseFloat(subtotalElement.innerText);
+subtotal= casePrice+phonePrice;
+subtotalElement.innerText = subtotal;
+
+//tax-------
+let taxElement =document.getElementById('tax');
+let tax = parseFloat(taxElement.innerText);
+tax = parseFloat((subtotal*0.1).toFixed(2));
+taxElement.innerText =  tax;
+
+//total-------
+let totalElement =document.getElementById('total');
+let total = parseFloat(totalElement.innerText);
+total = subtotal+ tax;
+totalElement.innerText = total ;
 
 
 
-    const taxElement = document.getElementById('tax');
-    const tax = parseFloat (phonePriceElement.innerText);
 
-    const totalElement = document.getElementById('total');
-    const total = parseFloat (totalElement.innerText);
-
-    subtotalElement.innerText=phonePrice+casePrice;
-
- taxElement.innerText =  (subtotal* 0.1).toFixed(2);
-
-totalElement.innerText = subtotal+ tax;
-
-
-    console.log(subtotal, phonePrice, ) ;
 })
